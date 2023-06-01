a5f7c72b8d1df0d03677e16faf7903549443aea2
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-06-01 09:17:10 -0700

    triangle strip

--- main.js
+++ main.js
@@ -38,31 +38,17 @@ function getKey(key) {
 }
 
 var vertices1 = [
-	vec2( -0.8, 0.8),
-	vec2(  .8, 0.8),
-	vec2(  .8, -0.8 ),
-
-
-
-	vec2( -0.8, 0.8),
-	vec2(  0.8, -0.8 ),
-	vec2( -0.8, -0.8 ),
-
-
+	vec2( -0.8, 0.1),
+	vec2(  .0, 0.1),
+	vec2(  -.8, -0.8 ),
+	vec2( 0.0, -0.8),
 ];
 
 var vertices2 = [
-	vec2( -0.7, 0.7),
-	vec2(  .7, 0.7),
-	vec2(  .7, -0.7 ),
-
-
-
-	vec2( -0.7, 0.7),
-	vec2(  0.7, -0.7 ),
-	vec2( -0.7, -0.7 ),
-
-
+	vec2( -0.9, 0.9),
+	vec2(  .9, 0.9),
+	vec2(  -.9, -0.0 ),
+	vec2( 0.9, -0.0),
 ];
 
 
@@ -75,31 +61,31 @@ window.onload = function init() {
 	gl.viewport( 0, 0, canvas.width, canvas.height );
 	gl.clearColor( 0.5, 0.5, 0.5, 1.0 );
 
+    program2 = initShaders( gl, "vertex-shader2", "fragment-shader2" );
+	gl.useProgram( program2 );
+	vBuffer2 = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer2);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices2), gl.STATIC_DRAW );
+	var vPosition2 = gl.getAttribLocation( program2, "vPosition2" );
+	gl.vertexAttribPointer( vPosition2, 2, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vPosition2 ); 
+    gl.clear( gl.COLOR_BUFFER_BIT ); 
+    gl.drawArrays( gl.TRIANGLE_STRIP, 0, 4 );
+
 
     program1 = initShaders( gl, "vertex-shader1", "fragment-shader1" );
 	gl.useProgram( program1 );
 	vBuffer1 = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer1);
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices1), gl.STATIC_DRAW );
-	var vPosition1 = gl.getAttribLocation( program1, "vPosition" );
+	var vPosition1 = gl.getAttribLocation( program1, "vPosition1" );
 	gl.vertexAttribPointer( vPosition1, 2, gl.FLOAT, false, 0, 0 );
 	gl.enableVertexAttribArray( vPosition1 );   
 	heightLoc = gl.getUniformLocation(program1, "height");
 	widthLoc = gl.getUniformLocation(program1, "width");
-    gl.clear( gl.COLOR_BUFFER_BIT ); 
-	gl.drawArrays( gl.TRIANGLES, 0, 6 );
+    render()
 
 
-    program2 = initShaders( gl, "vertex-shader2", "fragment-shader2" );
-	gl.useProgram( program2 );
-	vBuffer2 = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer2);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices2), gl.STATIC_DRAW );
-	var vPosition2 = gl.getAttribLocation( program2, "vPosition" );
-	gl.vertexAttribPointer( vPosition2, 2, gl.FLOAT, false, 0, 0 );
-	gl.enableVertexAttribArray( vPosition2 ); 
-    //gl.clear( gl.COLOR_BUFFER_BIT ); 
-	gl.drawArrays( gl.TRIANGLES, 0, 6 );
 	
 }
 
@@ -121,7 +107,8 @@ function render() {
 	document.getElementById("width").innerHTML = width;
 	gl.uniform1f(heightLoc, height);
 	gl.uniform1f(widthLoc, width);
-	gl.clear( gl.COLOR_BUFFER_BIT ); 
-	gl.drawArrays( gl.TRIANGLES, 0, 6 );
-	window.requestAnimFrame(render);
+	//gl.clear( gl.COLOR_BUFFER_BIT ); 
+	gl.drawArrays( gl.TRIANGLE_STRIP, 0, 4 );
+    
+	//window.requestAnimationFrame(render);
 }
