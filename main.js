376c2ba766b4743df11debb6fa202eb2d32eb0d2
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-06-01 09:43:15 -0700

    one object moving, one not

--- main.js
+++ main.js
@@ -62,7 +62,17 @@ window.onload = function init() {
 	gl.clearColor( 0.5, 0.5, 0.5, 1.0 );
 
     program2 = initShaders( gl, "vertex-shader2", "fragment-shader2" );
-	gl.useProgram( program2 );
+    program1 = initShaders( gl, "vertex-shader1", "fragment-shader1" );
+    render()
+
+	
+}
+
+function render() {
+    
+
+    
+    gl.useProgram( program2 );
 	vBuffer2 = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer2);
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices2), gl.STATIC_DRAW );
@@ -73,26 +83,9 @@ window.onload = function init() {
     gl.drawArrays( gl.TRIANGLE_STRIP, 0, 4 );
 
 
-    program1 = initShaders( gl, "vertex-shader1", "fragment-shader1" );
-	gl.useProgram( program1 );
-	vBuffer1 = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer1);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices1), gl.STATIC_DRAW );
-	var vPosition1 = gl.getAttribLocation( program1, "vPosition1" );
-	gl.vertexAttribPointer( vPosition1, 2, gl.FLOAT, false, 0, 0 );
-	gl.enableVertexAttribArray( vPosition1 );   
-	heightLoc = gl.getUniformLocation(program1, "height");
-	widthLoc = gl.getUniformLocation(program1, "width");
-    render()
-
-
-	
-}
-
-function render() {
-    
-	if (height > -1.8 && pressed == 1)
+    if (height > -1.8 && pressed == 1)
 		height = height - 0.01;
+    
 
 	if (height < 0 && pressed == 2)
 		height = height + 0.01;	
@@ -103,12 +96,19 @@ function render() {
 	if (width > -0.9 && pressed == 4)
 		width = width - 0.01;	
 	
-	document.getElementById("debug").innerHTML = height;
-	document.getElementById("width").innerHTML = width;
-	gl.uniform1f(heightLoc, height);
-	gl.uniform1f(widthLoc, width);
-	//gl.clear( gl.COLOR_BUFFER_BIT ); 
+
+    gl.useProgram( program1 );
+	vBuffer1 = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer1);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices1), gl.STATIC_DRAW );
+	var vPosition1 = gl.getAttribLocation( program1, "vPosition1" );
+	gl.vertexAttribPointer( vPosition1, 2, gl.FLOAT, false, 0, 0 );
+	gl.enableVertexAttribArray( vPosition1 );   
+	heightLoc = gl.getUniformLocation(program1, "height");
+	widthLoc = gl.getUniformLocation(program1, "width");
 	gl.drawArrays( gl.TRIANGLE_STRIP, 0, 4 );
+    gl.uniform1f(heightLoc, height);
+	gl.uniform1f(widthLoc, width);
     
-	//window.requestAnimationFrame(render);
+	window.requestAnimationFrame(render);
 }
