af3a274148c045aa9d2893ac1861fce5360be4c2
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-06-02 17:11:23 -0700

    timer

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ window.onload = async function init() {
     if ( !gl ) { alert( "WebGL isn't available" ); }
 	gl.viewport( 0, 0, canvas.width, canvas.height );
 	gl.clearColor( 0.5, 0.5, 0.5, 1.0 );
-
     program1 = initShaders( gl, "./shaders/dynamic.vert", "./shaders/yellow.frag" );
     program2 = initShaders( gl, "./shaders/dynamic.vert", "./shaders/blue.frag" );
+    setInterval(decrementTime,1000)
     render()
 }
 
@@ -25,4 +25,13 @@ const render = () => {
     gameLogic()
 
 	window.requestAnimationFrame(render);
+}
+const decrementTime = () => {
+    
+    var time = document.querySelector('#timer').innerHTML
+    if(Number(time) == 0){
+        return
+    }else{
+        document.querySelector('#timer').innerHTML = String(Number(time)-1)
+    }
 }
